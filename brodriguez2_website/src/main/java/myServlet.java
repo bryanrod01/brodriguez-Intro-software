a82fa38b15fc8b1bd5491dc676bf1a5aaf8c4c92
bryanrod01 <77567947+bryanrod01@users.noreply.github.com>
2024-03-20 21:36:52 -0500

    test

--- brodriguez2_website/src/main/java/myServlet.java
+++ brodriguez2_website/src/main/java/myServlet.java
@@ -1,12 +1,10 @@
 
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  * Servlet implementation class myServlet
  */
@@ -27,7 +25,7 @@ public class myServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello! from: ").append(request.getContextPath());
+		response.getWriter().append("Hello!").append(request.getContextPath());
 	}
 
 	/**
